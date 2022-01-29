9fc7a6d173d072d75eb3fc4c79876b1af35fa5d0
Adam Ashworth <aja65@cam.ac.uk>
2022-01-29 21:14:42 +0000

    started task 1b) distance function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,14 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+
+    outputList =[] #will append results into this
+    distance = 0 #working distance variable
+
+    for i in stations:
+        distance = math.sqrt( (i.coord[0]-p[0])**2 + (i.coord[1]-p[1])**2 ) # perform pythag calculation on x and y distances from station to input cooord p
+        outputList.append((i,distance))
+    
+    return outputList
