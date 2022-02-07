b9150d730fe56af7717f0de323291c1a8c3df360
Adam Ashworth <aja65@cam.ac.uk>
2022-02-07 18:06:32 +0000

    Completed Task 1C

--- Task1C.py
+++ Task1C.py
@@ -4,12 +4,20 @@ from floodsystem.stationdata import build_station_list
 def run():
     # Build list of stations
     stations = build_station_list()
-
+    
+    #Cambridge coordinate
     cambridge = (52.2053, 0.1218)
 
-    camStations=stations_within_radius(stations,cambridge,10)
-    stationNames=[ i.name for i in camStations]
-    print(stationNames)
+    #Find stations within 10km of coordinate
+    camStations=stations_within_radius(stations,cambridge,10) 
+    
+    #for each Station object in camStations, pick out the name and add it to a list
+    camStationsNames = [i.name for i in camStations]
+
+    #Sort list alphabetically
+    camStationsNames.sort() 
+
+    print(camStationsNames)
 
 if __name__ == "__main__":
     run()
