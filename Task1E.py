91f1a63ec5f40b47b98374904174b0f1932c77a7
kk757-student <kk757@cam.ac.uk>
2022-02-07 19:44:43 +0000

    reverted to og version

--- Task1E.py
+++ Task1E.py
@@ -1,16 +1,12 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 
-def s0(stations,N):
-    list0 = []
+def run():
+    N = 9
     stations = build_station_list()
-    
-    for i in stations:
-        list0.append(i,N)
- 
-    list0.sort(key=lambda y: y[1])
-    return list0
+    s0 = rivers_by_station_number(stations, N)
+    print(s0)
 
 if __name__ == "__main__":
     print("*** Task 1E: rivers by number of stations ***")
-
+run()
