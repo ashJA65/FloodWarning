f67306178e623db2cc28973caf4de158c62cb74d
kk757-student <kk757@cam.ac.uk>
2022-02-07 19:40:12 +0000

    still no changes to sorting of IE

--- Task1E.py
+++ Task1E.py
@@ -1,14 +1,16 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 
-def run():
-    N = 9
+def s0(stations,N):
+    list0 = []
     stations = build_station_list()
-    s0 = rivers_by_station_number(stations, N)
+    
+    for i in stations:
+        list0.append(i,N)
  
-    print(s0)
+    list0.sort(key=lambda y: y[1])
+    return list0
 
 if __name__ == "__main__":
     print("*** Task 1E: rivers by number of stations ***")
 
-run()
