aa020cb2f66e46a0e6abedcaf4a932f2a7bf11f6
Adam Ashworth <aja65@cam.ac.uk>
2022-02-08 13:33:58 +0000

    Added unit tests for station

--- test_station.py
+++ test_station.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import *
 
 
 def test_create_monitoring_station():
@@ -25,3 +25,45 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_typical_range_consistent():
+    #dummy data with correct range
+    assert MonitoringStation("1", "1", "sta1", (0., 1.), (1, 4), "River 1",
+                                 "Town 1").typical_range_consistent() == True
+
+    #test for missing data                             
+    assert MonitoringStation("11", "1", "sta1", (0., 1.), None, "River 1",
+                                 "Town 1").typical_range_consistent() == False
+
+    #test for max value being less than min value
+    assert MonitoringStation("1", "1", "sta1", (0., 1.), (1, -1), "River 1",
+                                 "Town 1").typical_range_consistent() == False
+    
+
+def test_inconsistent_typical_range_stations():
+    #dummy data with some faults
+    station1 = MonitoringStation(station_id="sta1",
+                                     measure_id="sta1",
+                                     label="Station A",
+                                     coord=(0, 1),
+                                     typical_range=(1., 4.),
+                                     river="River 1",
+                                     town="Town 1")
+    station2 = MonitoringStation(station_id="sta2",
+                                     measure_id="sta2",
+                                     label="Station B",
+                                     coord=(1, 1),
+                                     typical_range=None,
+                                     river="River 2",
+                                     town="Town 2")
+    station3 = MonitoringStation(station_id="sta3",
+                                     measure_id="sta3",
+                                     label="Station C",
+                                     coord=(0, 3),
+                                     typical_range=(1, -4),
+                                     river="River 2",
+                                     town="Town 3")
+
+    assert inconsistent_typical_range_stations([station1]) == []
+    assert inconsistent_typical_range_stations([station1, station2]) == [station2]
+    assert inconsistent_typical_range_stations([station2, station3]) == [station2, station3]
