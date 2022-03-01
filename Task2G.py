2545f9732194dbcc636c51bd41934f8f8f81c998
Adam Ashworth <aja65@cam.ac.uk>
2022-03-01 14:13:08 +0000

    Added additional output and some paramaterisation

--- Task2G.py
+++ Task2G.py
@@ -5,39 +5,22 @@ import matplotlib
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
 from floodsystem.analysis import polyfit
-
-
-def predicted_relative_water_level(station, prediction):
-    """return predicted water level as fraction of the typical range"""
-
-    #default case if data is not avaliable or inconsistent
-    relative_level = None
-
-    if station.typical_range_consistent():
-        try:
-            #should only work if data is consistent and avaliable
-            relative_level = (prediction - station.typical_range[0]) / (
-                station.typical_range[1] - station.typical_range[0])
-        except:
-            #if data is consistent but not
-            relative_level = None
-        
-    return relative_level    
-    
     
 
-def get_at_risk_towns(risk_thresholds):
+def get_at_risk_towns(risk_thresholds,predictionAmount,N):
+    """Print a list of towns that are predicted to have a increase in relative water level
+    N - amount of towns considered
+    predictionAmount - days into future predicted
+    risk_thresholds - adjusts wording
+    """
     #criteria: towns with stations that:
-    # are in the top 50 highest relative level
+    # are in the top 60 highest relative level
     # and are predicted to have a rise in relative water level
 
 
         # Build list of stations
     stations = build_station_list()
     update_water_levels(stations)
-
-    # Define N
-    N = 20
 
     # Setting the time interval to 2 days
     dt = 2
@@ -66,8 +49,11 @@ def get_at_risk_towns(risk_thresholds):
         # get the latest date
         latestDate = max(x - d0)
 
+        #amount of days to predict into the future
+        predictionAmount = 2
+
         # predict the water level two days later
-        prediction = poly(latestDate + 2)
+        prediction = poly(latestDate + predictionAmount)
 
         #get current relative water level
         r_level = station.relative_water_level()
@@ -94,10 +80,15 @@ def get_at_risk_towns(risk_thresholds):
     # sort at_risk_towns by risks in descending order
     at_risk_towns.sort(key=lambda x: x[1], reverse=True)
 
+
+    print("\nThe following warnings are for the top {} towns at risk, based on predictions for the next {} days".format(N,predictionAmount))
+    print("Warnings will not be generated for towns already underwater\n")
+
     print("Current risk thresholds: \n ")
     
     ratings = ['low', 'moderate', 'high', 'severe']
 
+    
     for i, threshold in enumerate(risk_thresholds):
         print("{} rating: {}".format(ratings[i+1],threshold))
 
@@ -113,9 +104,26 @@ def get_at_risk_towns(risk_thresholds):
             rating_factor = 3  # severe
         print('{}:\n\t{}'.format(town, ratings[rating_factor]))
 
+def predicted_relative_water_level(station, prediction):
+    """return predicted water level as fraction of the typical range"""
+
+    #default case if data is not avaliable or inconsistent
+    relative_level = None
+
+    if station.typical_range_consistent():
+        try:
+            #should only work if data is consistent and avaliable
+            relative_level = (prediction - station.typical_range[0]) / (
+                station.typical_range[1] - station.typical_range[0])
+        except:
+            #if data is consistent but not present return None
+            relative_level = None
+        
+    return relative_level    
+
 def run():
     thresholds=[0.5,5,10]
-    get_at_risk_towns(thresholds)
+    get_at_risk_towns(thresholds,2,60)
 if __name__ == "__main__":
     print("*** Task 2G final ***")
 
